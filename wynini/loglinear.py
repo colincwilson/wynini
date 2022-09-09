eb4c598f70c60b4d9f18778eea6283380daf7b49
Colin Wilson <colin.chris.wilson@gmail.com>
2022-09-09 18:44:56 -0400

    fixing loglinear

--- wynini/loglinear.py
+++ wynini/loglinear.py
@@ -17,12 +17,13 @@ def loglinear_weights(wfst, phi, w):
     w: [w_0, w_1, ...] (weight vector)
     """
     fst = wfst.fst
+    one = Weight('log', 0)
     for q in fst.states():
         q_arcs = fst.mutable_arcs(q)
         for t in q_arcs:
             _t = (q, t.ilabel, t.olabel, t.nextstate)
             if _t not in phi:
-                t.weight = Weight('log', 0)
+                t.weight = one
             else:
                 phi_t = phi.get(_t)
                 t.weight = Weight('log', np.dot(phi_t, w))
@@ -35,21 +36,33 @@ def loglinear_expected(wfst, phi, w):
     Expected violation counts of features/constraints in phi 
     given weights w.
     """
+    # Compute arc weights from Harmonies
     wfst = loglinear_weights(wfst, phi, w)
-
     fst = wfst.fst
-    d = shortestdistance(wfst.fst, reverse=True)
-    fst.reweight(d)
-    wfst.draw('tmp.dot')
 
+    # Forward potentials (sum over all paths from initial to q)
+    alpha = shortestdistance(fst, reverse=False)
+    alpha = [float(w) for w in alpha]
+    #print(alpha)
+
+    # Backward potentials (sum over all paths from q to finals)
+    beta = shortestdistance(fst, reverse=True)
+    beta = [float(w) for w in beta]
+    #print(beta)
+
+    # Accumulate expected violations across arcs
     n = w.shape[0]
     expect = np.zeros(n)
     for q in fst.states():
         for t in fst.arcs(q):
             _t = (q, t.ilabel, t.olabel, t.nextstate)
-            if _t not in phi:
+            if _t not in phi:  # all-zero violation vector
                 continue
-            plog = float(t.weight.to_string())
-            phi_t = phi.get(_t)
+            phi_t = phi.get(_t)  # violation vector
+            # plog of all paths through t
+            plog = alpha[q] + float(t.weight) + beta[t.nextstate]
             expect += np.exp(-plog) * phi_t
+
+    # Divide by partitition function (sum over all paths)
+    expect /= np.exp(-beta[0])
     return expect
