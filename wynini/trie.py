7a5a1bd1096ca16e518a93f85871a5b6a4507b8c
Colin Wilson <colin@cogsci.jhu.edu>
2024-04-22 17:10:28 -0400

    trie module

--- wynini/trie.py
+++ wynini/trie.py
@@ -4,34 +4,38 @@ from .wfst import Wfst
 from . import config
 
 
-def make_trie(vocab):
+def build_trie(vocab):
     """
-    Make trie from vocabulary of (word, token-list) pairs.
+    Build trie from vocabulary of (word, token-list) pairs.
     """
     trie = Wfst()
     q0 = trie.add_state()
     trie.set_start(q0)
-    epsilon = config.epsilon
-
-    for (word, toks) in vocab:
-        n = len(toks)
-        src = q0
-        for i in range(n):
-            tok = toks[i]
-            arc = None
-            dest = None
-            for _arc in trie.arcs(src):
-                if trie.ilabel(_arc) == tok:
-                    arc = _arc
-                    dest = _arc.nextstate
-                    break
-            if arc is None:
-                dest = trie.add_state()
-                #print(src, tok, epsilon, None, dest)
-                trie.add_arc(src, tok, epsilon, None, dest)
-            if i == (n - 1):
-                trie.set_state_label(dest, word)
-                trie.set_final(dest)
-            src = dest
+    _build_trie(trie, q0, vocab, 0)
 
     return trie
+
+
+def _build_trie(trie, src, vocab, i):
+    tok2dest = {}
+    dest2vocab = {}
+    epsilon = config.epsilon
+    for (word, toks) in vocab:
+        tok = toks[i]
+        if tok not in tok2dest:
+            dest = trie.add_state()
+            tok2dest[tok] = dest
+            dest2vocab[dest] = []
+        else:
+            dest = tok2dest[tok]
+
+        trie.add_arc(src, tok, epsilon, None, dest)
+
+        if len(toks) == (i + 1):
+            trie.set_state_label(dest, word)
+            trie.set_final(dest)
+        else:
+            dest2vocab[dest].append((word, toks))
+
+    for (dest, vocab_dest) in dest2vocab.items():
+        _build_trie(trie, dest, vocab_dest, i + 1)
